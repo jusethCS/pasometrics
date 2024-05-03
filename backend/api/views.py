86fc654e976f1b9fbe4f64d8be346efd7e284509
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-02 23:39:16 -0500

    Convert csv to pandas

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-#from django.shortcuts import render
+import pandas as pd
 import csv
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -7,17 +7,18 @@ from django.views.decorators.csrf import csrf_exempt
 def procesar_archivo_csv(request):
     if request.method == 'POST' and request.FILES.get('archivo'):
         archivo_csv = request.FILES['archivo']
-        # Lee el archivo CSV
-        datos = []
         try:
             decoded_file = archivo_csv.read().decode('utf-8')
             reader = csv.reader(decoded_file.splitlines())
-            for row in reader:
-                datos.append(row)
-            # Aquí puedes procesar los datos como desees
-            print(datos)
-            #
-            return JsonResponse({'mensaje': 'Archivo CSV procesado correctamente', 'datos': datos})
+            
+            # Convertir el CSV a un DataFrame de pandas
+            df = pd.DataFrame(reader, columns=next(reader))
+            
+            # Aquí puedes procesar el DataFrame según tus necesidades
+            print(df)
+            
+            # Puedes devolver el DataFrame en formato JSON si es necesario
+            return JsonResponse({'mensaje': 'Archivo CSV procesado correctamente', 'datos': df.to_json(orient='records')})
         except Exception as e:
             return JsonResponse({'error': 'Error al procesar el archivo CSV: {}'.format(str(e))}, status=400)
     else:
