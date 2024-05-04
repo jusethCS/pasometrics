a0e6c32ac0482cb53ea2390dbdd0d08f22e868c1
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-03 22:38:38 -0500

    Test connection

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,29 @@
 import os
 from .utils import get_data
+from sqlalchemy import create_engine
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
+
+# Enviromental variables
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv('DB_NAME')
+
+# Generate the conection token
+token = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@localhost:5432/{DB_NAME}"
 
 
 @csrf_exempt
 def process_csv_file(request):
     if request.method == 'POST':
         MT = get_data(request=request, filename="MT")
+        # test
+        db = create_engine(token)
+        con = db.connect()
+        MT.to_sql("prueba", con=con, if_exists='replace', index=True)
+        con.close()
         #print(MT)
-        print(os.getenv("DB_PASS"))
         return JsonResponse({'message': 'CSV file processed successfully'})
     else:
         json = {'error': 'Expected a POST request with attached files'}
