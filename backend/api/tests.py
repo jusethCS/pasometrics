613882a0a10dcd4aa130759964f4e9433569dc24
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-03 21:46:15 -0500

    Added unit test for process file

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+# API URL
+url = 'http://ec2-54-88-30-239.compute-1.amazonaws.com/upload/'
+
+# Setting the file and data
+csv_file = {
+    'MT': open('MT.csv', 'rb')
+}
+#data = {'clave': 'valor'}
+
+# Send POST request
+response = requests.post(url, files=csv_file)#, data=data)
+
+# Process the response
+if response.status_code == 200:
+    print("CSV file sent successfully")
+else:
+    print("Error sending CSV file:", response.text)
