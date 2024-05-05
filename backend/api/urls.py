60b2b90acd91e926c4cc6bea6a750dd89e4dc2f8
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-04 23:12:35 -0500

    Fixed url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import horse #process_csv_file, view_csv_data
+from .views import horse_crud #process_csv_file, view_csv_data
 
 urlpatterns = [
-    path('horse/', horse, name='horse')
+    path('horse/', horse_crud, name='horse')
     #path('upload/', process_csv_file, name='process_csv_file'),
     #path('get-data/', view_csv_data, name='view_csv_data'),
 ]
