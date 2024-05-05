bf44caf4fc2b79aa937e43846561e7104fdc6fbc
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-04 23:14:01 -0500

    Fixed safe method

--- backend/api/views.py
+++ backend/api/views.py
@@ -28,7 +28,7 @@ def horse_crud(request):
         con.close()
 
         # Response
-        return JsonResponse(data.to_json(orient='records'))
+        return JsonResponse(data.to_json(orient='records'), safe=False)
     
     if request.method == 'POST':
         # Retrieve horse name
@@ -44,4 +44,4 @@ def horse_crud(request):
         con.close()
 
         # Response: ID for inserted horse
-        return JsonResponse(data.to_json(orient='records'))
+        return JsonResponse(data.to_json(orient='records'), safe=False)
