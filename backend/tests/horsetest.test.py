c4ad2bad886ea9d1cc099827a7e89129f4c2dacd
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-05 22:49:38 -0500

    Added test for horsetest routine

--- backend/tests/horsetest.test.py
+++ backend/tests/horsetest.test.py
@@ -16,7 +16,7 @@ response.text
 # POST test: Insert into database
 data = {
     "horse": "ASD 364863 -C GD",
-    "date": "2024-05-05 15:00:00"
+    "datetime": "2024-05-05 15:00:00"
 }
 response = requests.post(url, data=data, headers={'auth': token})
 response.text
